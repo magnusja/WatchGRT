8ae3f80fd7b79cd37252d2f1f07dff6d4781d62e
jahnen <jahnen@in.tum.de>
2015-11-25 17:02:00 +0100

    Plain WatchApp Project

---
+++ WatchGRT/WatchGRT WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WatchGRT WatchKit Extension
+//
+//  Created by M J on 25/11/15.
+//  Copyright © 2015 jahnen. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
