62a96d1d4d492616d1f677e8ba47bb5b81f9a477
jahnen <jahnen@in.tum.de>
2015-12-09 16:21:24 +0100

    Add prediction code

--- WatchGRT/WatchGRT WatchKit Extension/PredictViewController.swift
+++ WatchGRT/WatchGRT WatchKit Extension/PredictViewController.swift
@@ -13,8 +13,37 @@ import WatchConnectivity
 class PredictInterfaceController: WKInterfaceController, WCSessionDelegate {
     @IBOutlet var currentFileLabel: WKInterfaceLabel!
     @IBOutlet var predictedClassLabel: WKInterfaceLabel!
+    @IBOutlet var sampleLabel: WKInterfaceLabel!
     
+    private let accelerometerManager = AccelerometerManager()
+    private let pipeline = GestureRecognitionPipeline()
+    private var sampleCounter = 0
+    private var isRunning = false
+    
+    private func startPrediction() {
+        sampleCounter = 0
+        isRunning = true
+        accelerometerManager.start { (x, y, z) -> Void in
+            let vector = VectorDouble()
+            vector.pushBack(x)
+            vector.pushBack(y)
+            vector.pushBack(z)
+            self.pipeline.predict(vector)
+            self.predictedClassLabel.setText("\(self.pipeline.predictedClassLabel)")
+            self.sampleLabel.setText("\(self.sampleCounter++)")
+        }
+    }
+    
+    private func stopPrediction() {
+        isRunning = false
+        accelerometerManager.stop()
+    }
     
     @IBAction func start() {
+        if isRunning {
+            stopPrediction()
+        } else {
+            startPrediction()
+        }
     }
 }
